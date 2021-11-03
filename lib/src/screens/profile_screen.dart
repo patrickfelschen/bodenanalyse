ed4ea56c8f96e2f927b73b310f78ff0a1934545d
Thomas Simpson <formelthomas@gmail.com>
2021-11-03 11:12:37 +0100

    Added ProfileScreen

---
+++ lib/src/screens/profile_screen.dart
@@ -0,0 +1,82 @@
+import 'package:bodenanalyse/src/screens/login_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  static const routeName = "/profileScreen";
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final String _name = "Michi Müller";
+  final String _mail = "michi.müller@acker.web";
+  final String _anzahlFelder = "25";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              // TODO: change to image
+              width: 200.0,
+              height: 200.0,
+              decoration: const BoxDecoration(
+                color: Colors.lightGreen,
+                shape: BoxShape.circle,
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                  _name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24.0,
+                  ),
+                ),
+                Text(_mail, style: const TextStyle(fontWeight: FontWeight.bold),),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Text(
+                    _anzahlFelder,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                const Text(
+                    'Anzahl der Felder',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                  minimumSize: Size(400, 50),
+                ),
+                child: Text('Abmelden'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
