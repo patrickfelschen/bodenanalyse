4654e837241b4171480a1b734dc02a02a2982d40
Patrick Felschen <patrick@felschen.de>
2021-11-03 15:28:54 +0100

    new

--- lib/src/screens/registration_screen.dart
+++ lib/src/screens/registration_screen.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
-import 'package:bodenanalyse/src/screens/home_screen.dart';
+import 'package:bodenanalyse/src/providers/auth_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
@@ -28,119 +27,176 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    body:
-        Container(
-            decoration: BoxDecoration(
-            image: DecorationImage(
-            image: AssetImage("assets/images/start.png"),
-              fit: BoxFit.cover,
-            ),),
+    final AuthProvider _authProvider = Provider.of<AuthProvider>(context);
 
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 60),
-                child:
-                Image.asset("assets/images/logoStart.png"),
-              ),
-
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: 40,right:40,top:30
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("NAME",style:TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),),
-                      SizedBox(height: 5,),
-                      TextField(
-                        controller: textController1,
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
-                          fillColor: Colors.white70 ,filled: true,
-                        ),
-                      ),
-                      SizedBox(height: 30,),
-
-                      Text("E-MAIL",style:TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),),
-                      SizedBox(height: 5,),
-                      TextField(
-                        controller: textController2,
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
-                          fillColor: Colors.white70 ,filled: true,
-                        ),
-                      ),
-                      SizedBox(height: 30,),
-
-                      Text("PASSWORT",style:TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),),
-                      SizedBox(height: 5,),
-                      TextField(
-                        controller: textController3,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
-                          fillColor: Colors.white70 ,filled: true,
-                        ),
-                      ),
-                      SizedBox(height: 30,)
-                    ],
-                  ),
-                ),
-
-                Flexible(
-                  child: MaterialButton(
-                      minWidth: 200,
-                      height:60,
-                    onPressed: (){
-                      //todo: hier checken, ob Daten schon registriert, wenn nein -> Navigator.push
-                      Navigator.pushNamed(context, HomeScreen.routeName);
-                    },
-                      color: Theme.of(context).colorScheme.primary,
-                      child:
-                        Text("REGISTRIEREN",style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 23,
-                        ),),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    ),
-                  ),
-
-              Flexible(
-                child: MaterialButton(
-                  minWidth: 200,
-                  height:60,
-                  onPressed: (){Navigator.pop(context);},
-                  //color: Color(0xFF8BA94D),
-                  child:
-                  Text("zurück",style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 23,
-                  ),),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                ),
-              ),
-              ],
-            ),
+    void _showSnackbar(String s) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(s),
         ),
       );
+    }
 
+    void signUp() {
+      String username = textController1.text.toString().trim();
+      String email = textController2.text.toString().trim();
+      String password = textController3.text.toString().trim();
+
+      if (username.isEmpty) {
+        _showSnackbar("Namen eingeben!");
+        return;
+      }
+
+      if (email.isEmpty) {
+        _showSnackbar("E-Mail eingeben!");
+        return;
+      }
+
+      if (password.isEmpty) {
+        _showSnackbar("Passwort eingeben!");
+        return;
+      }
+
+      _authProvider.signUp(
+        username: username,
+        email: email,
+        password: password,
+      );
+
+      //Navigator.pushNamed(context, HomeScreen.routeName);
+    }
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/start.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 60),
+              child: Image.asset("assets/images/logoStart.png"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 40, right: 40, top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "NAME",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: textController1,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "E-MAIL",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: textController2,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "PASSWORT",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextField(
+                    controller: textController3,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                      fillColor: Colors.white70,
+                      filled: true,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              child: MaterialButton(
+                minWidth: 200,
+                height: 60,
+                onPressed: () => signUp(),
+                color: Theme.of(context).colorScheme.primary,
+                child: Text(
+                  "REGISTRIEREN",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 23,
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+            Flexible(
+              child: MaterialButton(
+                minWidth: 200,
+                height: 60,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                //color: Color(0xFF8BA94D),
+                child: Text(
+                  "zurück",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 23,
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
 }
