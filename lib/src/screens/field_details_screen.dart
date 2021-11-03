f110a18f53ef034ec66ee1675c7b8f48966cfebb
Patrick Felschen <patrick@felschen.de>
2021-11-03 15:29:23 +0100

    reformat

--- lib/src/screens/field_details_screen.dart
+++ lib/src/screens/field_details_screen.dart
@@ -1,7 +1,7 @@
 import 'package:bodenanalyse/src/screens/analysis_start_screen.dart';
 import 'package:bodenanalyse/src/screens/edit_field_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:easy_search/easy_search.dart';
+import 'package:flutter/material.dart';
 
 class FieldDetailsScreen extends StatefulWidget {
   const FieldDetailsScreen({Key? key}) : super(key: key);
@@ -20,57 +20,55 @@ class _FieldDetailsScreenState extends State<FieldDetailsScreen> {
         title: Text("Felddetails"), //todo: Name und ArtOberboden in Titel
         actions: [
           MaterialButton(
-            child:
-              Text("Bearbeiten", style: TextStyle(
+            child: Text(
+              "Bearbeiten",
+              style: TextStyle(
                 color: Colors.white,
-              ),),
-            onPressed: (){Navigator.pushNamed(context, EditFieldScreen.routeName);},
+              ),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, EditFieldScreen.routeName);
+            },
           ),
         ],
       ),
-      body:
-      Container(
-        child:
-          Column(
-            children: [
-              EasySearch( //von: https://pub.dev/packages/easy_search
-                searchResultSettings: SearchResultSettings(
-                  padding: EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
-                ),
-                controller: SearchItem(
-                  items: [
-                    //todo: Liste der Bewertungen, die aus DB abgerufen wurde
-                   /* Item(ModelExample(name: 'Tiago', age: 36), false),
+      body: Container(
+          child: Column(
+        children: [
+          EasySearch(
+            //von: https://pub.dev/packages/easy_search
+            searchResultSettings: SearchResultSettings(
+              padding: EdgeInsets.only(left: 8.0, top: 8.0, right: 8.0),
+            ),
+            controller: SearchItem(
+              items: [
+                //todo: Liste der Bewertungen, die aus DB abgerufen wurde
+                /* Item(ModelExample(name: 'Tiago', age: 36), false),
                     Item(ModelExample(name: 'Mel', age: 3), false),
                     Item(ModelExample(name: 'Monique', age: 30), false),*/
-                  ],
-                ),
-              ),
-
-
-
-            ],
-          )
-      ),
-
+              ],
+            ),
+          ),
+        ],
+      )),
       floatingActionButton: startAnalysis(context),
     );
   }
 }
 
-
-
-Widget startAnalysis(context){
+Widget startAnalysis(context) {
   return SizedBox(
     width: 350,
-    child:
-    FloatingActionButton.extended(
-      onPressed: (){Navigator.pushNamed(context, AnalysisStartScreen.routeName);},
-      label: const Text('Bewertung starten', style: TextStyle(color: Colors.white),),
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
+    child: FloatingActionButton.extended(
+      onPressed: () {
+        Navigator.pushNamed(context, AnalysisStartScreen.routeName);
+      },
+      label: const Text(
+        'Bewertung starten',
+        style: TextStyle(color: Colors.white),
       ),
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
     ),
   );
 }
